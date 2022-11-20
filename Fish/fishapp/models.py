7f9d58df032e6f372459ba2f7969ab40551b6ea9
Aleksey <Deep1985@yandex.ru>
2022-11-21 01:43:35 +0300

    model Fish str

--- Fish/fishapp/models.py
+++ Fish/fishapp/models.py
@@ -3,8 +3,11 @@ from django.db import models
 
 class Fish(models.Model):
     title = models.CharField(max_length=255)
-    context = models.TextField(blank=True)
+    content = models.TextField(blank=True)
     photo = models.ImageField(upload_to='photo/%Y/%m/%d')
     time_create = models.DateTimeField(auto_now_add=True)
     time_update = models.DateTimeField(auto_now=True)
     is_published = models.BooleanField(default=True)
+
+    def __str__(self):
+        return self.title
