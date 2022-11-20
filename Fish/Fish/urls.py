7c91f050d09abaff34e6c5cf1006eea9597ce037
Aleksey <Deep1985@yandex.ru>
2022-11-20 23:55:55 +0300

    models

--- Fish/Fish/urls.py
+++ Fish/Fish/urls.py
@@ -13,11 +13,13 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+from django.conf.urls.static import static
 from django.contrib import admin
 from django.urls import path
 
 from django.urls import include
 
+from Fish import settings
 from fishapp.views import pageNotFound
 
 urlpatterns = [
@@ -25,5 +27,6 @@ urlpatterns = [
     path('', include('fishapp.urls')),
 
 ]
-
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
 handler404 = pageNotFound
