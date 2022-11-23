89282a1a3d147001ea7cd78497e2ee30032868ed
Aleksey <Deep1985@yandex.ru>
2022-11-23 03:12:13 +0300

    Теги

--- Fish/fishapp/views.py
+++ Fish/fishapp/views.py
@@ -4,20 +4,10 @@ from django.shortcuts import render, redirect
 from .models import *
 
 
-menu = [{'title': "О сайте", 'url_name': 'about'},
-        {'title': "Добавить статью", 'url_name': 'add_page'},
-        {'title': "Обратная связь", 'url_name': 'contact'},
-        {'title': "Войти", 'url_name': 'login'}
-        ]
-
-
 def index(request):
     posts = Fish.objects.all()
-    cats = Category.objects.all()
     context = {
         'posts': posts,
-        'cats': cats,
-        'menu': menu,
         'title': 'Главная страница',
         'cat_selected': 0
     }
@@ -25,7 +15,7 @@ def index(request):
 
 
 def about(request):
-    return render(request, 'fishapp/about.html', {'menu': menu, 'title': 'О сайте'})
+    return render(request, 'fishapp/about.html', {'title': 'О сайте'})
 
 
 def addpage(request):
@@ -46,17 +36,15 @@ def show_post(request, post_id):
 
 def show_category(request, cat_id):
     posts = Fish.objects.filter(cat_id=cat_id)
-    cats = Category.objects.all()
     if len(posts) == 0:
         raise Http404
     context = {
         'posts': posts,
-        'cats': cats,
-        'menu': menu,
         'title': 'Отображение по рубрикам',
         'cat_selected': cat_id
     }
     return render(request, 'fishapp/index.html', context=context)
+
 
 def pageNotFound(request, exception):
     return HttpResponseNotFound('<h1>Страница не найдена.</h1>')
