026c18e25668bab8b57c766e8b798a6337226dc1
Aleksey <Deep1985@yandex.ru>
2022-11-28 00:17:30 +0300

    debug

--- Fish/Fish/urls.py
+++ Fish/Fish/urls.py
@@ -27,9 +27,12 @@ urlpatterns = [
     path('', include('fishapp.urls')),
 
 ]
-#urlpatterns += static(settings.STATIC_URL, document_root=settings.BASE_DIR)
+# urlpatterns += static(settings.STATIC_URL, document_root=settings.BASE_DIR)
 
 if settings.DEBUG:
+    urlpatterns = [
+        path('__debug__/', include('debug_toolbar.urls')),
+    ] + urlpatterns
     urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
 
 handler404 = pageNotFound
