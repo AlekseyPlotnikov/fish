3164045e222659e5360052dbe0137936c9e615fe
Aleksey <Deep1985@yandex.ru>
2022-11-27 23:01:37 +0300

    Регистрация

--- Fish/fishapp/views.py
+++ Fish/fishapp/views.py
@@ -143,3 +143,14 @@ def show_category(request, cat_slug):
 
 def pageNotFound(request, exception):
     return HttpResponseNotFound('<h1>Страница не найдена.</h1>')
+
+
+class RegisterUser(DataMixin, CreateView):
+    form_class = RegisterUserForm
+    template_name = 'fishapp/register.html'
+    success_url = reverse_lazy('login')
+
+    def get_context_data(self, *, object_list=None, **kwargs):
+        context = super().get_context_data(**kwargs)
+        c_def = self.get_user_context(title='Регистрация')
+        return dict(list(context.items()) + list(c_def.items()))
